008bb6bb48f7eb518c74eb4e6432c7e55b4766b6
broxy-a <abdrahmanovamir2001@gmail.com>
2025-02-24 19:26:06 +0500

    first commit

--- src/App.java
+++ src/App.java
@@ -1,14 +1,27 @@
 import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+
 
 public class App {
     public static boolean isPalindrome(String text) {
        String input = (text.toLowerCase());
-       return input.equals(StringUtils.reverse(input));
+       String reversed = StringUtils.reverse(input);
+       
+       return input.equals(reversed);
 
 
     }   
     public static void main(String[] args) {
-      System.out.println(App.isPalindrome("Ага"));
+      //System.out.println(App.isPalindrome("Ага"));
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Введите слово для проверки на палиндром: ");
 
+      
+      String input = scanner.nextLine();
+      System.out.println("Результат " + isPalindrome(input));
+
+      scanner.close();
+
+      
     }
 }
